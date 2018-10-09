112bf8e7d1bafdfe7d7e77a2bbd474ec16065592
yanfeng12 <32252305+yanfeng12@users.noreply.github.com>
2018-10-09 10:09:36 +0800

    关于我们

---
+++ pages/products/myimg.js
@@ -0,0 +1,86 @@
+// pages/products/myimg.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array:[{
+      mode:'aspectFit',
+      text:'1',
+      src:'/images/i1.jpg'
+      },
+      {
+        mode: 'aspectFit',
+        text: '2',
+        src: '/images/i2.jpg'
+      },
+      {
+        mode: 'aspectFit',
+        text: '3',
+        src: '/images/i3.jpg'
+      },
+      {
+        mode: 'aspectFit',
+        text: '4',
+        src: '/images/i4.jpg'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
